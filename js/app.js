c6f4e4a61b562c6bb25f5d7cb93eaa1cff64c787
marahuyo <going2email2@gmail.com>
2021-08-29 15:54:21 +0800

    Make toolbox buttons function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 import CartesianGraph from './cartesian-graph.js';
 
 window.addEventListener('load', () => {
+  // Setting up the toolbox.
+
+  const operations = document.querySelectorAll(
+    '#toolbox div.button-set input[type="button"]'
+  );
+  let active = operations[0];
+  for (let i = 0; i < operations.length; i++) {
+    operations[i].addEventListener('click', (e) => {
+      active.classList.remove('button--active-state');
+      active = e.currentTarget;
+      active.classList.add('button--active-state');
+    });
+  }
+
   // Setting up the viewport.
   const viewport = document.getElementsByTagName('main')[0];
 
