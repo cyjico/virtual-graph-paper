c393476f35363214d9bf719895c1ffce1f04bb37
marahuyo <going2email2@gmail.com>
2021-08-31 21:18:31 +0800

    Update GUI

--- js/app.js
+++ js/app.js
@@ -25,128 +25,142 @@ function getSelectedOperation() {
 }
 
 window.addEventListener('load', () => {
-  // Setting up the toolbox.
-
-  const operations = document.querySelectorAll(
-    '#toolbox div.button-set input[type="button"]'
-  );
-  let active = operations[0];
-  for (let i = 0; i < operations.length; i++) {
-    operations[i].addEventListener('click', (e) => {
-      active.classList.remove('button--active-state');
-      active = e.currentTarget;
-      active.classList.add('button--active-state');
-    });
-  }
-
-  // Setting up the viewport.
   const viewport = document.getElementsByTagName('main')[0];
-
   const operationHistory = new OperationHistory(viewport);
   const cartesianGraph = new CartesianGraph(viewport);
 
-  /** @type {import('./operations/operation.js').default} */
-  let currentOperation = null;
-  const input = {
-    isLeftMouseDown: false,
-    isWheelMouseDown: false,
-    relativeCursorPosition: {
-      x: 0,
-      y: 0,
-    },
-    keys: [],
-  };
-
-  document.addEventListener('keydown', (e) => {
-    if (!input.keys.includes(e.code)) {
-      input.keys.push(e.code);
+  {
+    // Setting up the toolbox.
+    const buttons = document.querySelectorAll(
+      '#toolbox div.button-set input[type="button"]'
+    );
+    let active = buttons[0];
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', (e) => {
+        active.classList.remove('button--active-state');
+        active = e.currentTarget;
+        active.classList.add('button--active-state');
+      });
     }
+  }
 
-    if (input.keys[0].includes('Control')) {
-      if (input.keys.length == 2 && input.keys[1] == 'KeyZ') {
-        operationHistory.undoOperation();
-      } else if (
-        input.keys.length == 3 &&
-        input.keys[1].includes('Shift') &&
-        input.keys[2] == 'KeyZ'
-      ) {
-        operationHistory.redoOperation();
+  {
+    // Setting up the buttons of the right panel..
+    const buttons = document.querySelectorAll(
+      '#right-panel .button-set input[type="button"]'
+    );
+    buttons[0].addEventListener('click', () => {
+      operationHistory.undoOperation();
+    });
+    buttons[1].addEventListener('click', () => {
+      operationHistory.redoOperation();
+    });
+  }
+
+  {
+    /** @type {import('./operations/operation.js').default} */
+    let currentOperation = null;
+    const input = {
+      isLeftMouseDown: false,
+      isWheelMouseDown: false,
+      relativeCursorPosition: {
+        x: 0,
+        y: 0,
+      },
+      keys: [],
+    };
+
+    document.addEventListener('keydown', (e) => {
+      if (!input.keys.includes(e.code)) {
+        input.keys.push(e.code);
       }
-    }
-  });
 
-  document.addEventListener('keyup', (e) => {
-    for (let i = 0; i < input.keys.length; i++) {
-      if (input.keys[i] === e.code) {
-        input.keys.splice(i, 1);
-        break;
+      if (input.keys[0].includes('Control')) {
+        if (input.keys.length == 2 && input.keys[1] == 'KeyZ') {
+          operationHistory.undoOperation();
+        } else if (
+          input.keys.length == 3 &&
+          input.keys[1].includes('Shift') &&
+          input.keys[2] == 'KeyZ'
+        ) {
+          operationHistory.redoOperation();
+        }
       }
-    }
-  });
+    });
 
-  viewport.addEventListener('mousedown', (e) => {
-    input.isLeftMouseDown = (e.buttons & 1) != 0;
-    input.isWheelMouseDown = (e.buttons & 4) != 0;
+    document.addEventListener('keyup', (e) => {
+      for (let i = 0; i < input.keys.length; i++) {
+        if (input.keys[i] === e.code) {
+          input.keys.splice(i, 1);
+          break;
+        }
+      }
+    });
 
-    if (input.isLeftMouseDown && !currentOperation) {
-      currentOperation = new (getSelectedOperation())(
-        operationHistory,
-        cartesianGraph
-      );
-      currentOperation.mousedown(e, input);
-    }
-  });
+    viewport.addEventListener('mousedown', (e) => {
+      input.isLeftMouseDown = (e.buttons & 1) != 0;
+      input.isWheelMouseDown = (e.buttons & 4) != 0;
 
-  viewport.addEventListener('mousemove', (e) => {
-    const bounds = e.currentTarget.getBoundingClientRect();
-    const rx = cartesianGraph.scaleDownX(e.clientX - bounds.left);
-    const ry = cartesianGraph.scaleDownY(e.clientY - bounds.top);
+      if (input.isLeftMouseDown && !currentOperation) {
+        currentOperation = new (getSelectedOperation())(
+          operationHistory,
+          cartesianGraph
+        );
+        currentOperation.mousedown(e, input);
+      }
+    });
 
-    if (input.isWheelMouseDown) {
-      cartesianGraph.offset.x -= (input.relativeCursorPosition.x - rx) * 0.95;
-      cartesianGraph.offset.y -= (input.relativeCursorPosition.y - ry) * 0.95;
-      operationHistory.render();
-      cartesianGraph.render();
-    }
+    viewport.addEventListener('mousemove', (e) => {
+      const bounds = e.currentTarget.getBoundingClientRect();
+      const rx = cartesianGraph.scaleDownX(e.clientX - bounds.left);
+      const ry = cartesianGraph.scaleDownY(e.clientY - bounds.top);
 
-    input.relativeCursorPosition.x = rx;
-    input.relativeCursorPosition.y = ry;
+      if (input.isWheelMouseDown) {
+        cartesianGraph.offset.x -= (input.relativeCursorPosition.x - rx) * 0.95;
+        cartesianGraph.offset.y -= (input.relativeCursorPosition.y - ry) * 0.95;
+        operationHistory.render();
+        cartesianGraph.render();
+      }
 
-    if (input.isLeftMouseDown && currentOperation) {
-      currentOperation.mousemove(e, input);
-    }
-  });
+      input.relativeCursorPosition.x = rx;
+      input.relativeCursorPosition.y = ry;
 
-  document.addEventListener('mouseup', (e) => {
-    input.isLeftMouseDown = input.isLeftMouseDown && (e.buttons & 1) != 0;
-    input.isWheelMouseDown = input.isWheelMouseDown && (e.buttons & 4) != 0;
+      if (input.isLeftMouseDown && currentOperation) {
+        currentOperation.mousemove(e, input);
+      }
+    });
 
-    if (!input.isLeftMouseDown && currentOperation) {
-      currentOperation.mouseup(e, input);
-      operationHistory.addOperation(currentOperation);
-      currentOperation = null;
-    }
-  });
+    document.addEventListener('mouseup', (e) => {
+      input.isLeftMouseDown = input.isLeftMouseDown && (e.buttons & 1) != 0;
+      input.isWheelMouseDown = input.isWheelMouseDown && (e.buttons & 4) != 0;
 
-  viewport.addEventListener(
-    'wheel',
-    (e) => {
-      cartesianGraph.scale = Math.min(
-        Math.max(
-          cartesianGraph.scale -
-            e.deltaY *
-              0.0075 *
-              (cartesianGraph.scale / cartesianGraph.baseScale),
-          4.5
-        ),
-        75.825
-      );
+      if (!input.isLeftMouseDown && currentOperation) {
+        currentOperation.mouseup(e, input);
+        operationHistory.addOperation(currentOperation);
+        currentOperation = null;
+      }
+    });
 
-      cartesianGraph.render();
-      operationHistory.render();
-    },
-    { passive: true }
-  );
+    viewport.addEventListener(
+      'wheel',
+      (e) => {
+        cartesianGraph.scale = Math.min(
+          Math.max(
+            cartesianGraph.scale -
+              e.deltaY *
+                0.0075 *
+                (cartesianGraph.scale / cartesianGraph.baseScale),
+            4.5
+          ),
+          75.825
+        );
+
+        cartesianGraph.render();
+        operationHistory.render();
+      },
+      { passive: true }
+    );
+  }
 
   cartesianGraph.render();
 });
