a61abe82269efc5757e10afc63ca648a8ee7f4a2
marahuyo <going2email2@gmail.com>
2021-09-03 15:00:57 +0800

    Update DrawArcText

--- js/operations/draw-arc-text.js
+++ js/operations/draw-arc-text.js
@@ -19,14 +19,33 @@ class DrawArcText extends DrawArc {
   }
 
   mousemove(args) {
-    const endRad = -(this.endRad > 0 ? this.endRad - Math.PI * 2 : this.endRad);
-    this.text = `${(Math.round(endRad * (180 / Math.PI) * 100) / 100).toFixed(
+    const PI2 = Math.PI * 2;
+    const CCW_SIGN = this.counterClockwise ? 1 : -1;
+
+    const startRad =
+      (this.counterClockwise && this.startRad > 0) ||
+      (!this.counterClockwise && this.startRad < 0)
+        ? this.startRad - PI2 * CCW_SIGN
+        : this.startRad;
+    const endRad = (() => {
+      const baseRad =
+        (this.counterClockwise && this.endRad > 0) ||
+        (!this.counterClockwise && this.endRad < 0)
+          ? this.endRad - PI2 * CCW_SIGN
+          : this.endRad;
+
+      return (
+        baseRad - (Math.abs(baseRad) <= Math.abs(startRad) ? PI2 * CCW_SIGN : 0)
+      );
+    })();
+    const rangeRad = endRad - startRad;
+    const medianRad = rangeRad / 2;
+
+    this.text = `${(Math.round(rangeRad * (180 / Math.PI) * 100) / 100).toFixed(
       2
     )}°`;
-
-    const rad = endRad / 2;
-    this.textOffset.x = Math.cos(rad);
-    this.textOffset.y = -Math.sin(rad);
+    this.textOffset.x = Math.cos(this.startRad + medianRad) * 2;
+    this.textOffset.y = Math.sin(this.startRad + medianRad) * CCW_SIGN;
 
     super.mousemove.call(this, args);
   }
@@ -36,21 +55,27 @@ class DrawArcText extends DrawArc {
 
     switch (this.text) {
       case '90.00°':
-        context.beginPath();
-        const radius = this.radius * this.cartesianGraph.scale;
-        const cx = this.cartesianGraph.scaleUpX(this.center.x);
-        const cy = this.cartesianGraph.scaleUpY(this.center.y);
-        context.moveTo(cx, cy - radius);
-        context.lineTo(cx + radius, cy - radius);
-        context.lineTo(cx + radius, cy);
-        context.lineTo(cx + radius, cy);
-        context.stroke();
+        {
+          context.beginPath();
+          const sign = this.counterClockwise ? 1 : -1;
+          const radius = this.radius * this.cartesianGraph.scale;
+          const cx = this.cartesianGraph.scaleUpX(this.center.x);
+          const cy = this.cartesianGraph.scaleUpY(this.center.y);
+
+          context.moveTo(cx, cy - radius * sign);
+          context.lineTo(cx + radius * sign, cy - radius * sign);
+          context.lineTo(cx + radius * sign, cy);
+          context.lineTo(cx + radius * sign, cy);
+          context.stroke();
+        }
         break;
       default:
         super.render.call(this);
         break;
     }
 
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
     context.fillText(
       this.text,
       this.cartesianGraph.scaleUpX(this.center.x + this.textOffset.x),
