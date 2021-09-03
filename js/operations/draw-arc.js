18ae3272e4dcb172d4979d2d6194e180287e2e4c
marahuyo <going2email2@gmail.com>
2021-09-03 15:00:30 +0800

    Add arc degree and direction switching

--- js/operations/draw-arc.js
+++ js/operations/draw-arc.js
@@ -19,7 +19,9 @@ class DrawArc extends Operation {
       y: 0,
     };
     this.radius = 0;
+    this.startRad = 0;
     this.endRad = 0;
+    this.counterClockwise = true;
 
     this.strokeWidth = 0;
   }
@@ -36,8 +38,34 @@ class DrawArc extends Operation {
     this.radius = Math.sqrt(x * x + y * y);
     this.endRad = Math.atan2(y, x);
 
+    const PI2 = Math.PI * 2;
+    if ((this.endRad / PI2) % 1 == (this.startRad / PI2) % 1) {
+      this.endRad =
+        this.startRad + Math.PI * 2 * (this.counterClockwise ? -1 : 1);
+    }
+
     this.operationManager.render();
     this.render();
+  }
+
+  keydown({ input }) {
+    if (input.keys.length == 1) {
+      switch (input.keys[0]) {
+        case 'KeyQ':
+          this.startRad = Math.atan2(
+            input.relativeCursorPosition.y - this.center.y,
+            input.relativeCursorPosition.x - this.center.x
+          );
+          this.operationManager.render();
+          this.render();
+          break;
+        case 'KeyE':
+          this.counterClockwise = !this.counterClockwise;
+          this.operationManager.render();
+          this.render();
+          break;
+      }
+    }
   }
 
   render() {
@@ -48,9 +76,9 @@ class DrawArc extends Operation {
       this.cartesianGraph.scaleUpX(this.center.x),
       this.cartesianGraph.scaleUpY(this.center.y),
       this.radius * this.cartesianGraph.scale,
-      0,
+      this.startRad,
       this.endRad,
-      true
+      this.counterClockwise
     );
     context.lineWidth = this.strokeWidth;
     context.stroke();
